6d0c479ec370ace8d8ae6a01475779f7aa77ad99
ksowah <sowahkelvin640@gmail.com>
2022-09-25 16:36:34 +0000

    Update profile

---
+++ screens/AccountSetup/UpdateProfile.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import tw from 'twrnc'
+
+const UpdateProfile = () => {
+  return (
+    <View style={tw`flex`}>
+      <Text style={tw`flex`}>UpdateProfile</Text>
+    </View>
+  );
+}
+
+
+export default UpdateProfile;
