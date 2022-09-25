8fbafafad09f444c38f1601d580d7cc193b83cf9
ksowah <sowahkelvin640@gmail.com>
2022-09-25 16:36:13 +0000

    home

---
+++ screens/Home.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const Home = () => {
+  return (
+    <View>
+        <Text>
+            Home
+        </Text>
+    </View>
+  )
+}
+
+export default Home
