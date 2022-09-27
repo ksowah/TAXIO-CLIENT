ee58cff725274a4ec7d0bb3e5954ebdc2d9d8d76
ksowah <sowahkelvin640@gmail.com>
2022-09-27 23:01:29 +0000

    update profile screen

--- screens/AccountSetup/UpdateProfile.tsx
+++ screens/AccountSetup/UpdateProfile.tsx
@@ -1,13 +1,25 @@
-import { View, Text } from 'react-native';
-import tw from 'twrnc'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  Keyboard,
+} from "react-native";
+import tw from "twrnc";
+import Button from "../../components/Button";
+import { Logout } from "../../components/helpers/logout";
 
-const UpdateProfile = () => {
+const UpdateProfile = ({navigation}: any) => {
   return (
-    <View style={tw`flex`}>
-      <Text style={tw`flex`}>UpdateProfile</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={tw`flex-1 w-full`}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SafeAreaView style={tw`flex-1 bg-[#181A20]`}>
+          <Button title="Logout" onPress={() => Logout(navigation)}/>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
-}
-
+};
 
 export default UpdateProfile;
