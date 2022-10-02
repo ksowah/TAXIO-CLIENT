de57c6c282be388da9fd99e209c092b01ec19157
ksowah <sowahkelvin640@gmail.com>
2022-10-02 14:48:21 +0000

    add bookings screen

---
+++ screens/Home/Bookings.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import tw from 'twrnc'
+
+const Bookings = () => {
+  return (
+    <View style={tw`flex`}>
+      <Text style={tw`flex`}>Bookings</Text>
+    </View>
+  );
+}
+
+
+export default Bookings;
