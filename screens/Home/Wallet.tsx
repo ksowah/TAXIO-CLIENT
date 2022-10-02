e359c3ca7b29bb38b6cd838457cb4dcacc975036
ksowah <sowahkelvin640@gmail.com>
2022-10-02 14:49:12 +0000

    add wallet screen

---
+++ screens/Home/Wallet.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import tw from 'twrnc'
+
+const Wallet = () => {
+  return (
+    <View style={tw`flex`}>
+      <Text style={tw`flex`}>Wallet</Text>
+    </View>
+  );
+}
+
+
+export default Wallet;
