ecaebb14dc9141c911dc6cfd4f3093595ec169ab
ksowah <sowahkelvin640@gmail.com>
2022-10-04 23:14:32 +0000

    add theme configuration to map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,17 +1,42 @@
-import { View, StyleSheet, Dimensions } from "react-native";
+import { useContext } from "react"
+import { View, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import tw from "twrnc";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 import { darkThemeMap } from "../utils/mapStyles";
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import {EventRegister} from "react-native-event-listeners";
+import themeContext from "./config/themeContext";
+import { Theme } from "../types";
 
-const Map = () => {
+
+interface Props {
+  onPress: () => void;
+  theme: Theme;
+  Icon: any;
+}
+
+const MapTabs = ({onPress, theme, Icon}: Props) => {
+  return (
+    <TouchableOpacity activeOpacity={.5} onPress={onPress} style={tw`mr-2 rounded-full items-center justify-center h-12 w-12 bg-[${theme.map_items}] shadow-lg`}>
+    { Icon }
+    </TouchableOpacity>
+  )
+}
+const Map = ({navigation}: any) => {
+
   const [position, setPosition] = useState({
     latitude: 0,
     longitude: 0,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+
+  const [switchTheme, setSwitchTheme] = useState(true)
+
+  const theme: Theme = useContext(themeContext)
 
   useEffect(() => {
     (async () => {
@@ -32,15 +57,17 @@ const Map = () => {
     })();
   }, []);
 
-  
+  const changeTheme = () => {
+    setSwitchTheme(!switchTheme)
+    EventRegister.emit('switchTheme', switchTheme)
+    }
 
   return (
     <>
       <MapView
         style={styles.map}
-        mapType="mutedStandard"
         initialRegion={position}
-        customMapStyle={darkThemeMap}
+        customMapStyle={!switchTheme ? darkThemeMap : undefined}
         provider={ PROVIDER_GOOGLE }
       >
         <Marker
@@ -53,6 +80,13 @@ const Map = () => {
           identifier="origin"
         />
       </MapView>
+
+          <View style={tw`absolute top-[55px] right-6 flex-row items-center`}>
+             <MapTabs onPress={() => navigation.navigate("SearchLocation")} theme={theme} Icon={<Feather name="search" size={24} color={theme.text} />}/>
+             <MapTabs onPress={changeTheme} theme={theme} Icon={<Feather name="bell" size={24} color={theme.text} />}/>
+             <MapTabs onPress={changeTheme} theme={theme} Icon={<FontAwesome name="moon-o" size={24} color={theme.text} />}/>
+          </View>
+     
     </>
   );
 };
