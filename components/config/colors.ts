4612d8128a013883f7c47cb4e8acb98b56ffd1ae
ksowah <sowahkelvin640@gmail.com>
2022-10-04 23:15:56 +0000

    define colors for theme

---
+++ components/config/colors.ts
@@ -0,0 +1,27 @@
+const theme = {
+  light: {
+    base: "#FFFFFF",
+    text: "#212121",
+    border: "#f8f8f8",
+    input_base: "#fafafa",
+    yellow: "#febb1b",
+    fade_yellow: "#ffefca",
+    fade_text: "#cbcbcb",
+    map_items: "#ffe4a4",
+    mode: false
+  },
+  dark: {
+    base: "#181a20",
+    text: "#fff",
+    border: "#24272f",
+    input_base: "#35383f",
+    yellow: "#febb1b",
+    fade_text: "#bbbcbd",
+    map_items: "#febb1b",
+    fade_yellow: "#524322",
+    mode: true
+  },
+};
+
+
+export default theme;
