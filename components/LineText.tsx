c3fe68a8f2dfa7cadcd225ef5ff34ccdefc824f1
ksowah <sowahkelvin640@gmail.com>
2022-10-04 23:14:05 +0000

    add theme to line

--- components/LineText.tsx
+++ components/LineText.tsx
@@ -1,14 +1,20 @@
 import { View, Text } from 'react-native';
 import tw from 'twrnc'
+import { Theme } from '../types';
+import themeContext from './config/themeContext';
+import { useContext } from "react"
 
 interface Props {
     text: string
 }
 
 const LineText = ({text}: Props) => {
+
+  const theme: Theme = useContext(themeContext)
+
   return (
-    <View style={tw`flex w-full border-b border-[#282a31] items-center my-4 justify-center`}>
-      <Text style={tw`text-white text-[16px] -mb-2 px-1 bg-[#181a20]`}>{text}</Text>
+    <View style={tw`flex w-full border-b border-[${theme.border}] items-center my-4 justify-center`}>
+      <Text style={tw`text-[${theme.text}] text-[16px] -mb-2 px-1 bg-[${theme.base}]`}>{text}</Text>
     </View>
   );
 }
