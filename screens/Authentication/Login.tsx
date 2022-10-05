292486d1b502c0dfaa92daf2d0f229e5305104b8
ksowah <sowahkelvin640@gmail.com>
2022-10-05 23:17:32 +0000

    add theme configuration

--- screens/Authentication/Login.tsx
+++ screens/Authentication/Login.tsx
@@ -9,6 +9,9 @@ import tw from "twrnc";
 import { FontAwesome5 } from "@expo/vector-icons";
 import Button from "../../components/Button";
 import LineText from "../../components/LineText";
+import { Theme } from "../../types";
+import themeContext from "../../components/config/themeContext";
+import { useContext } from "react";
 
 interface Props {
   provider: string;
@@ -17,29 +20,33 @@ interface Props {
 }
 
 const AuthProvider = ({ provider, Icon, onPress }: Props) => {
+
+  const theme: Theme = useContext(themeContext);
+
   return (
     <TouchableOpacity
       onPress={onPress}
       activeOpacity={0.5}
-      style={tw`w-full flex-row items-center justify-center h-13 bg-[#1F222A] rounded-2xl mb-3`}
+      style={tw`w-full flex-row items-center justify-center h-13 bg-[${theme.input_base}] border border-[${theme.border}] rounded-2xl mb-3`}
     >
       {Icon}
-      <Text style={tw`text-white`}>{provider}</Text>
+      <Text style={tw`text-[${theme.text}] font-bold`}>{provider}</Text>
     </TouchableOpacity>
   );
 };
 
 const Login = ({ navigation }: any) => {
 
+  const theme: Theme = useContext(themeContext);
 
   return (
-    <SafeAreaView style={tw`flex-1 bg-[#181A20] items-center justify-center`}>
+    <SafeAreaView style={tw`flex-1 bg-[${theme.base}] items-center justify-center`}>
       <View>
-        <Image source={require("../../assets/login.png")} />
+        <Image source={theme.mode ? require("../../assets/login.png") : require("../../assets/light-login.png")} />
       </View>
 
       <View style={tw`px-4 mb-6`}>
-        <Text style={tw`text-white text-5xl`}>Let's you in</Text>
+        <Text style={tw`text-[${theme.text}] text-5xl`}>Let's you in</Text>
       </View>
 
       <View style={tw`w-full px-4`}>
@@ -70,7 +77,7 @@ const Login = ({ navigation }: any) => {
             <FontAwesome5
               name="apple"
               size={26}
-              color="#fff"
+              color={theme.text}
               style={tw`mr-2`}
             />
           }
@@ -87,7 +94,7 @@ const Login = ({ navigation }: any) => {
         title="Sign in with password"
       />
 
-      <Text style={tw`text-white mt-6`}>
+      <Text style={tw`text-[${theme.fade_text}] mt-6`}>
         Don't have an account?
         <Text
           style={tw`text-[#c18f1c]`}
