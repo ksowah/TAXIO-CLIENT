32ffdc6c74a6556638a0a0dd61652db8dfe6a624
ksowah <sowahkelvin640@gmail.com>
2022-10-06 23:49:08 +0000

    furder build bottom sheet

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -5,6 +5,7 @@ import {
   Animated,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
 import { useContext } from "react";
 import tw from "twrnc";
@@ -19,21 +20,52 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import themeContext from "./config/themeContext";
 import { Theme } from "../types";
-import PlacesInputAutoComplete from "./PlacesInputAutoComplete";
 import { Feather } from "@expo/vector-icons";
+import PickupAutoComplete from "./Home/PickupAutoComplete";
+import DestinationAutocomplete from "./Home/DestinationAutocomplete";
+import { useRecoilValue } from "recoil";
+import {
+  addressAtom,
+  destinationAtom,
+  originAtom,
+  travelTimeInfo,
+} from "./atoms/tripAtom";
+import Button from "./Button";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const BottomSheetComponent = ({navigation}: any) => {
+const BottomSheetComponent = ({ navigation }: any) => {
   // implement bottom sheet
   const ref1 = useRef<BottomSheet>(null);
   const ref2 = useRef<BottomSheet>(null);
   const snapPoints1 = useMemo(() => ["15%", "90%"], []);
-  const snapPoints2 = useMemo(() => ["50%"], []);
+  const snapPoints2 = useMemo(() => ["45%", "85%"], []);
   const [sheetSnapPoint, setsheetSnapPoint] = useState<any>(0);
-  const [isSelected, setIsSelected] = useState(false);
+  const [isDestinationSelected, setIsDestinationSelected] =
+    useState<any>(false);
 
   const fadeAnim = useRef(new Animated.Value(0)).current;
 
   const theme: Theme = useContext(themeContext);
+
+  const origin = useRecoilValue<any>(originAtom);
+
+  const destination = useRecoilValue<any>(destinationAtom);
+
+  const travelDistance = useRecoilValue<any>(travelTimeInfo);
+
+  const travelDistanceKM = travelDistance?.distance?.text;
+
+  const address = useRecoilValue<any>(addressAtom);
+
+  const addressSlice = address?.split(",")[0];
+
+  const [choseNewOrigin, setChoseNewOrigin] = useState(false);
+
+  const originSlice = origin?.description.split(",")[0] 
+  const destinationSlice = destination?.description
+    .split(" ")
+    .slice(0, 2)
+    .join(" ");
 
   // callbacks
   const handleSheetChanges = useCallback((index: number) => {
@@ -45,6 +77,7 @@ const BottomSheetComponent = ({navigation}: any) => {
     ref1.current?.expand();
   }, []);
 
+
   useEffect(() => {
     Animated.timing(fadeAnim, {
       toValue: 1,
@@ -55,7 +88,127 @@ const BottomSheetComponent = ({navigation}: any) => {
 
   return (
     <View style={tw`flex-1`}>
-      <Map navigation={navigation}/>
+      <Map navigation={navigation} />
+
+      {isDestinationSelected && (
+        <BottomSheet
+          ref={ref2}
+          index={0}
+          snapPoints={snapPoints2}
+          handleStyle={tw`bg-[${theme.base}] rounded-t-3xl`}
+          handleIndicatorStyle={tw`bg-[${theme.border}]`}
+          backgroundStyle={tw`bg-[${theme.base}] shadow-xl`}
+        >
+          <BottomSheetView style={tw`h-full bg-[${theme.base}]`}>
+            <View style={tw`px-3 items-center `}>
+              <View
+                style={tw`py-2 w-full border-b shadow-2xl flex-row items-center justify-between border-[${theme.border}]`}
+              >
+                <Text style={tw`text-[${theme.text}] font-bold text-2xl`}>
+                  Distance
+                </Text>
+
+                <Text style={tw`text-[${theme.text}] text-[1rem]`}>
+                  {travelDistanceKM}
+                </Text>
+              </View>
+
+              <View
+                style={tw`py-6 flex-row border-b mb-4 border-[${theme.border}]`}
+              >
+                <View style={tw`h-[8rem] justify-evenly items-center`}>
+                  <View
+                    style={tw`bg-[${theme.fade_yellow}] h-12 w-12 rounded-full items-center justify-center`}
+                  >
+                    <View
+                      style={tw`bg-[${theme.yellow}] h-8 w-8 rounded-full items-center justify-center`}
+                    >
+                      <Foundation name="target-two" size={20} color="#35353f" />
+                    </View>
+                  </View>
+
+                  <Ionicons
+                    name="ellipsis-vertical"
+                    size={26}
+                    color="#9e9e9e"
+                  />
+
+                  <View
+                    style={tw`bg-[${theme.fade_yellow}] h-12 w-12 rounded-full items-center justify-center`}
+                  >
+                    <View
+                      style={tw`bg-[${theme.yellow}] h-8 w-8 rounded-full items-center justify-center`}
+                    >
+                      <FontAwesome5
+                        name="map-marker-alt"
+                        size={20}
+                        color="#35353f"
+                      />
+                    </View>
+                  </View>
+                </View>
+
+                <View style={tw`relative flex-1 pl-2`}>
+                  <View style={tw`flex-1 mb-4 flex-row items-center `}>
+                    {/* --------// */}
+                    <View style={tw`flex-1`}>
+                      <Text
+                        style={tw`text-[${theme.text}] text-[1.3rem] font-bold`}
+                      >
+                        {originSlice || addressSlice}
+                      </Text>
+                      <Text style={tw`text-[${theme.fade_text}] text-[.9rem]`}>
+                        {origin?.description || address}
+                      </Text>
+                    </View>
+
+                    <TouchableOpacity
+                      activeOpacity={0.5}
+                      style={tw`h-full items-center justify-center`}
+                    >
+                      <MaterialCommunityIcons
+                        name="lead-pencil"
+                        size={24}
+                        color={theme.yellow}
+                      />
+                    </TouchableOpacity>
+                  </View>
+
+                  <View style={tw`flex-1 flex-row items-center`}>
+                    {/* --------// */}
+                    <View style={tw`flex-1`}>
+                      <Text
+                        style={tw`text-[${theme.text}] text-[1.3rem] font-bold`}
+                      >
+                        {destinationSlice}
+                      </Text>
+                      <Text style={tw`text-[${theme.fade_text}] text-[.9rem]`}>
+                        {destination?.description}
+                      </Text>
+                    </View>
+
+                    <TouchableOpacity
+                      activeOpacity={0.5}
+                      style={tw`h-full items-center justify-center`}
+                    >
+                      <MaterialCommunityIcons
+                        name="lead-pencil"
+                        size={24}
+                        color={theme.yellow}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+
+              <Button
+                title="Continue to Order"
+                onPress={() => navigation.navigate("SelectRide")}
+              />
+            </View>
+          </BottomSheetView>
+        </BottomSheet>
+      )}
 
       <BottomSheet
         ref={ref1}
@@ -63,8 +216,9 @@ const BottomSheetComponent = ({navigation}: any) => {
         snapPoints={snapPoints1}
         onChange={handleSheetChanges}
         handleStyle={tw`bg-[${theme.base}] rounded-t-3xl`}
-        handleIndicatorStyle={tw`bg-[#9e9e9e]`}
+        handleIndicatorStyle={tw`bg-[${theme.border}]`}
         backgroundStyle={tw`bg-[${theme.base}] shadow-xl`}
+        style={tw`${isDestinationSelected && "hidden"}`}
       >
         <BottomSheetView style={tw`h-full bg-[${theme.base}]`}>
           <Input
@@ -73,65 +227,117 @@ const BottomSheetComponent = ({navigation}: any) => {
             placeholder={"Where would you go?"}
             placeholderTextColor={"#797a7c"}
             autoCapitalize="none"
-            keyboardType="email-address"
-            textContentType="emailAddress"
-            autoComplete="email"
             inputStyle={tw`text-white`}
             autoCorrect={false}
             returnKeyType="next"
             onFocus={handleChangeSnapPoint}
             rightIcon={<Foundation name="marker" size={24} color="#9e9e9e" />}
-            // onChangeText={(email) => setEmail(email)}
-            // value={email}
           />
 
           {sheetSnapPoint === 1 && (
             <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            <Animated.View
-              style={[tw`w-full flex-1 px-4`, { opacity: fadeAnim }]}
-            >
-              
-              <View style={tw`py-4 border-b border-[${theme.border}]`}>
-                <Text
-                  style={tw`text-[${theme.text}] text-center font-bold text-2xl`}
-                >
-                  Select Address
-                </Text>
-              </View>
-  
-              <View style={tw`py-6 flex-row border-b border-[${theme.border}]`}>
-                <View style={tw`h-[8rem] justify-evenly items-center`}>
-                  <FontAwesome5 name="dot-circle" size={26} color="#febb1b" />
-                  <Ionicons name="ellipsis-vertical" size={26} color="#9e9e9e" />
-                  <FontAwesome5 name="map-marker-alt" size={26} color="#febb1b" />
-                </View>
-  
-                <View style={tw`relative flex-1`}>
-                  <View style={tw`flex-1 mb-4`}>
-                    <PlacesInputAutoComplete />
-                  </View>
-                  <View style={tw`flex-1`}>
-                    <PlacesInputAutoComplete />
-                  </View>
-                </View>
-              </View>
-  
-              <View
-                style={[
-                  tw`flex-row items-center justify-between py-6 border-b border-[${theme.border}]`,
-                  { zIndex: -10 },
-                ]}
+              <Animated.View
+                style={[tw`w-full flex-1 px-4`, { opacity: fadeAnim }]}
               >
-                <View style={tw`flex-row items-center`}>
-                  <FontAwesome name="bookmark" size={24} color="#febb1b" />
-                  <Text style={tw`text-[${theme.text}] font-bold text-xl ml-4`}>
-                    Saved Places
+                <View
+                  style={tw`py-2 border-b shadow-2xl  border-[${theme.border}]`}
+                >
+                  <Text
+                    style={tw`text-[${theme.text}] text-center font-bold text-2xl`}
+                  >
+                    Select Address
                   </Text>
                 </View>
-                <Entypo name="chevron-right" size={24} color="#febb1b" />
-              </View>
-            </Animated.View>
-          </TouchableWithoutFeedback>
+
+                <View
+                  style={tw`py-6 flex-row border-b border-[${theme.border}]`}
+                >
+                  <View style={tw`h-[8rem] justify-evenly items-center`}>
+                    <FontAwesome5 name="dot-circle" size={26} color="#febb1b" />
+                    <Ionicons
+                      name="ellipsis-vertical"
+                      size={26}
+                      color="#9e9e9e"
+                    />
+                    <FontAwesome5
+                      name="map-marker-alt"
+                      size={26}
+                      color="#febb1b"
+                    />
+                  </View>
+
+                  <View style={tw`relative flex-1 pl-2`}>
+                    {choseNewOrigin ? (
+                      <View
+                        style={tw`flex-1 border border-[${theme.yellow}] mb-4 flex-row items-center justify-center bg-[${theme.input_base}] rounded-2xl`}
+                      >
+                        <PickupAutoComplete />
+                        <TouchableOpacity
+                          activeOpacity={0.5}
+                          style={tw`w-10 h-full items-center justify-center`}
+                          onPress={() => setChoseNewOrigin(false)}
+                        >
+                          <Foundation
+                            name="target-two"
+                            size={22}
+                            color={theme.fade_text}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                      
+                    ) : (
+                      <TouchableOpacity activeOpacity={.8} onPress={() => setChoseNewOrigin(true)} style={tw`flex-1 border border-[${theme.yellow}] mb-4 flex-row items-center justify-between bg-[${theme.input_focus}] rounded-2xl`}>
+                        <Text style={tw`text-[${theme.text}] text-[1rem] ml-2`}>Current Location</Text>
+                        <View
+                          style={tw`w-10 h-full items-center justify-center`}
+                        >
+                          <Foundation
+                            name="target-two"
+                            size={22}
+                            color={theme.fade_text}
+                          />
+                        </View>
+                      </TouchableOpacity>
+                    )}
+
+                    <View
+                      style={tw`flex-1 border border-[${theme.yellow}] flex-row items-center bg-[${theme.input_base}] rounded-2xl`}
+                    >
+                      <DestinationAutocomplete
+                        setIsDestinationSelected={setIsDestinationSelected}
+                      />
+                      <TouchableOpacity
+                        activeOpacity={0.5}
+                        style={tw`w-10 h-full items-center justify-center`}
+                      >
+                        <FontAwesome5
+                          name="map-marker-alt"
+                          size={22}
+                          color={theme.fade_text}
+                        />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View>
+                {/* ------------// */}
+                <View
+                  style={[
+                    tw`flex-row items-center justify-between py-6 border-b border-[${theme.border}]`,
+                    { zIndex: -10 },
+                  ]}
+                >
+                  <View style={tw`flex-row items-center`}>
+                    <FontAwesome name="bookmark" size={24} color="#febb1b" />
+                    <Text
+                      style={tw`text-[${theme.text}] font-bold text-xl ml-4`}
+                    >
+                      Saved Places
+                    </Text>
+                  </View>
+                  <Entypo name="chevron-right" size={24} color="#febb1b" />
+                </View>
+              </Animated.View>
+            </TouchableWithoutFeedback>
           )}
         </BottomSheetView>
       </BottomSheet>
