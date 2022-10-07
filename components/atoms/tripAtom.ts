4ab9ab4712dcb491a1fb1812c7d6a7e635829554
ksowah <sowahkelvin640@gmail.com>
2022-10-07 23:49:52 +0000

    add atoms for trip

---
+++ components/atoms/tripAtom.ts
@@ -0,0 +1,23 @@
+import { atom } from "recoil"
+
+export const originAtom = atom({
+    key: "originAtom",
+    default: null,
+})
+
+export const destinationAtom = atom({
+    key: "destinationAtom",
+    default: null,
+})
+
+export const travelTimeInfo = atom({
+    key: "travelTimeInfo",
+    default: null,
+})
+
+export const addressAtom = atom({
+    key: "addressAtom",
+    default: null,
+})
+
+
