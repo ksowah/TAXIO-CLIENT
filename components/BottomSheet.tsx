fd71f5c922a684e7e53bf0e98444865bf063244c
ksowah <sowahkelvin640@gmail.com>
2022-10-08 18:20:50 +0000

    a little clean up

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -32,6 +32,7 @@ import {
 } from "./atoms/tripAtom";
 import Button from "./Button";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import DestinationSelectedBottomSheet from "./bottomSheetUtils/DestinationSelectedBottomSheet";
 
 const BottomSheetComponent = ({ navigation }: any) => {
   // implement bottom sheet
@@ -91,123 +92,7 @@ const BottomSheetComponent = ({ navigation }: any) => {
       <Map navigation={navigation} />
 
       {isDestinationSelected && (
-        <BottomSheet
-          ref={ref2}
-          index={0}
-          snapPoints={snapPoints2}
-          handleStyle={tw`bg-[${theme.base}] rounded-t-3xl`}
-          handleIndicatorStyle={tw`bg-[${theme.border}]`}
-          backgroundStyle={tw`bg-[${theme.base}] shadow-xl`}
-        >
-          <BottomSheetView style={tw`h-full bg-[${theme.base}]`}>
-            <View style={tw`px-3 items-center `}>
-              <View
-                style={tw`py-2 w-full border-b shadow-2xl flex-row items-center justify-between border-[${theme.border}]`}
-              >
-                <Text style={tw`text-[${theme.text}] font-bold text-2xl`}>
-                  Distance
-                </Text>
-
-                <Text style={tw`text-[${theme.text}] text-[1rem]`}>
-                  {travelDistanceKM}
-                </Text>
-              </View>
-
-              <View
-                style={tw`py-6 flex-row border-b mb-4 border-[${theme.border}]`}
-              >
-                <View style={tw`h-[8rem] justify-evenly items-center`}>
-                  <View
-                    style={tw`bg-[${theme.fade_yellow}] h-12 w-12 rounded-full items-center justify-center`}
-                  >
-                    <View
-                      style={tw`bg-[${theme.yellow}] h-8 w-8 rounded-full items-center justify-center`}
-                    >
-                      <Foundation name="target-two" size={20} color="#35353f" />
-                    </View>
-                  </View>
-
-                  <Ionicons
-                    name="ellipsis-vertical"
-                    size={26}
-                    color="#9e9e9e"
-                  />
-
-                  <View
-                    style={tw`bg-[${theme.fade_yellow}] h-12 w-12 rounded-full items-center justify-center`}
-                  >
-                    <View
-                      style={tw`bg-[${theme.yellow}] h-8 w-8 rounded-full items-center justify-center`}
-                    >
-                      <FontAwesome5
-                        name="map-marker-alt"
-                        size={20}
-                        color="#35353f"
-                      />
-                    </View>
-                  </View>
-                </View>
-
-                <View style={tw`relative flex-1 pl-2`}>
-                  <View style={tw`flex-1 mb-4 flex-row items-center `}>
-                    {/* --------// */}
-                    <View style={tw`flex-1`}>
-                      <Text
-                        style={tw`text-[${theme.text}] text-[1.3rem] font-bold`}
-                      >
-                        {originSlice || addressSlice}
-                      </Text>
-                      <Text style={tw`text-[${theme.fade_text}] text-[.9rem]`}>
-                        {origin?.description || address}
-                      </Text>
-                    </View>
-
-                    <TouchableOpacity
-                      activeOpacity={0.5}
-                      style={tw`h-full items-center justify-center`}
-                    >
-                      <MaterialCommunityIcons
-                        name="lead-pencil"
-                        size={24}
-                        color={theme.yellow}
-                      />
-                    </TouchableOpacity>
-                  </View>
-
-                  <View style={tw`flex-1 flex-row items-center`}>
-                    {/* --------// */}
-                    <View style={tw`flex-1`}>
-                      <Text
-                        style={tw`text-[${theme.text}] text-[1.3rem] font-bold`}
-                      >
-                        {destinationSlice}
-                      </Text>
-                      <Text style={tw`text-[${theme.fade_text}] text-[.9rem]`}>
-                        {destination?.description}
-                      </Text>
-                    </View>
-
-                    <TouchableOpacity
-                      activeOpacity={0.5}
-                      style={tw`h-full items-center justify-center`}
-                    >
-                      <MaterialCommunityIcons
-                        name="lead-pencil"
-                        size={24}
-                        color={theme.yellow}
-                      />
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-
-              <Button
-                title="Continue to Order"
-                onPress={() => navigation.navigate("SelectRide")}
-              />
-            </View>
-          </BottomSheetView>
-        </BottomSheet>
+        <DestinationSelectedBottomSheet navigation={navigation}/>
       )}
 
       <BottomSheet
