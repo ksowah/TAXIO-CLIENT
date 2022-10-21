6076f722c5ce4ad4fb30b05668472cc3040372c9
Kelvin Sowah <sowahkelvin640@gmail.com>
2022-10-21 23:55:01 +0000

    create fake driver details

---
+++ components/bookings/fakeDriverDetails.ts
@@ -0,0 +1,62 @@
+export const driverDetails = [
+    {
+    name: "Kelvin Sowah",
+    car: "Honda Mobillo",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/kevin.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver1.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver2.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver3.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver4.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver6.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver7.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver8.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver9.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    car: "Toyota Alfard",
+    plate: "HDG 6374 SY",
+    image: require("../../assets/drivers/driver10.jpg"),
+  },
+]
