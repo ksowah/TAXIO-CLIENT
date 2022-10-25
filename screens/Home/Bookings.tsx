6d8187497a6f3df12e0c8466ca1ddfaddb265907
Kelvin Sowah <sowahkelvin640@gmail.com>
2022-10-25 10:25:11 +0000

    disintegrate component and reuse

--- screens/Home/Bookings.tsx
+++ screens/Home/Bookings.tsx
@@ -5,6 +5,7 @@ import themeContext from '../../components/config/themeContext';
 import { Theme } from '../../types';
 import { Feather } from '@expo/vector-icons';
 import TopTabs from '../../components/bookings';
+import TabsHeader from '../../components/TabsHeader';
 
 
 const Bookings = () => {
@@ -13,20 +14,7 @@ const Bookings = () => {
 
   return (
     <SafeAreaView style={tw`flex-1 bg-[${theme.base}]`}>
-      <View style={tw`flex-row items-center justify-between p-4`}>
-        <View style={tw`flex-row items-center`}>
-          <Image source={require("../../assets/bookings.png")}/>
-          <Text style={tw`text-[${theme.text}] font-bold text-[1.5rem] ml-2`}>My Bookings</Text>
-        </View>
-
-        <View style={tw`flex-row items-center`}>
-          <Feather name="search" size={24} color={theme.text} />
-          <TouchableOpacity activeOpacity={.5} style={tw`border-2 ml-4 border-[${theme.text}] rounded-full`}>
-            <Feather name="more-horizontal" size={18} color={theme.text} />
-          </TouchableOpacity>
-        </View>
-      </View>
-
+      <TabsHeader title='My Bookings' />
       <TopTabs />
     </SafeAreaView>
   );
