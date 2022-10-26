4547d306f6cf3a910f355356bd302d11b4524996
Kelvin Sowah <sowahkelvin640@gmail.com>
2022-10-26 14:19:29 +0000

    add new key to fake chat details

--- components/bookings/fakeChatDetails.ts
+++ components/bookings/fakeChatDetails.ts
@@ -1,41 +1,53 @@
-export const chatDetails = [
+import { Chat } from "../../types";
+
+export const chatDetails: Chat[] = [
     {
+    newChat: true,
     name: "Kelvin Sowah",
     image: require("../../assets/kevin.jpg"),
   },
   {
+    newChat: true,
     name: "Albert Flores",
     image: require("../../assets/drivers/driver1.jpg"),
   },
   {
+    newChat: false,
     name: "Cornor Rayse",
     image: require("../../assets/drivers/driver2.jpg"),
   },
   {
+    newChat: true,
     name: "Jeff Pim",
     image: require("../../assets/drivers/driver3.jpg"),
   },
   {
+    newChat: false,
     name: "Klay Forbs",
     image: require("../../assets/drivers/driver4.jpg"),
   },
   {
+    newChat: false,
     name: "Phil Jackson",
     image: require("../../assets/drivers/driver6.jpg"),
   },
   {
+    newChat: true,
     name: "Kevin Hart",
     image: require("../../assets/drivers/driver7.jpg"),
   },
   {
+    newChat: false,
     name: "Jay Dex",
     image: require("../../assets/drivers/driver8.jpg"),
   },
   {
+    newChat: false,
     name: "Paul George",
     image: require("../../assets/drivers/driver9.jpg"),
   },
   {
+    newChat: false,
     name: "George Hill",
     image: require("../../assets/drivers/driver10.jpg"),
   },
