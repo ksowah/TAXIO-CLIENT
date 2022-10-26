a9fef525838b849bdf3ab63ce5d846d04d806550
Kelvin Sowah <sowahkelvin640@gmail.com>
2022-10-26 14:12:12 +0000

    create fake chats

---
+++ components/bookings/fakeChatDetails.ts
@@ -0,0 +1,42 @@
+export const chatDetails = [
+    {
+    name: "Kelvin Sowah",
+    image: require("../../assets/kevin.jpg"),
+  },
+  {
+    name: "Albert Flores",
+    image: require("../../assets/drivers/driver1.jpg"),
+  },
+  {
+    name: "Cornor Rayse",
+    image: require("../../assets/drivers/driver2.jpg"),
+  },
+  {
+    name: "Jeff Pim",
+    image: require("../../assets/drivers/driver3.jpg"),
+  },
+  {
+    name: "Klay Forbs",
+    image: require("../../assets/drivers/driver4.jpg"),
+  },
+  {
+    name: "Phil Jackson",
+    image: require("../../assets/drivers/driver6.jpg"),
+  },
+  {
+    name: "Kevin Hart",
+    image: require("../../assets/drivers/driver7.jpg"),
+  },
+  {
+    name: "Jay Dex",
+    image: require("../../assets/drivers/driver8.jpg"),
+  },
+  {
+    name: "Paul George",
+    image: require("../../assets/drivers/driver9.jpg"),
+  },
+  {
+    name: "George Hill",
+    image: require("../../assets/drivers/driver10.jpg"),
+  },
+]
