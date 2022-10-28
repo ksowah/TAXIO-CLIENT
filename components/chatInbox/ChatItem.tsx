9a6b332a0ba23ddacdf714f87dbb4f3d60b0e7d7
Kelvin Sowah <sowahkelvin640@gmail.com>
2022-10-28 14:56:40 +0000

    create props type

--- components/chatInbox/ChatItem.tsx
+++ components/chatInbox/ChatItem.tsx
@@ -6,7 +6,16 @@ import { useContext } from 'react';
 import { Avatar } from '@rneui/base';
 import { AntDesign } from '@expo/vector-icons';
 
-const ChatItem = ({chat, onPress, chats} : {chat: Chat, onPress?: () => void, chats?: Boolean}) => {
+interface Props {
+    chat: Chat;
+    chats?: boolean;
+    onPress?: () => void;
+    newChat?: boolean;
+    message?: string;
+    count?: string;
+}
+
+const ChatItem = ({ chat, onPress, chats, newChat, message, count }: Props) => {
 
     const theme: Theme = useContext(themeContext);
 
@@ -21,21 +30,24 @@ const ChatItem = ({chat, onPress, chats} : {chat: Chat, onPress?: () => void, ch
 
                 <View>
                     <Text style={tw`text-[${theme.text}] font-bold text-[1.2rem] ml-4`}>{chat.name}</Text>
-                    <Text style={tw`text-[${theme.fade_text}] text-[.8rem] ml-4 mt-2`}>Incoming | Dec 19, 2024</Text>
+                    <Text style={tw`text-[${theme.fade_text}] text-[.8rem] ml-4 mt-2`}>{message}</Text>
                 </View>
             </View>
 
             <View style={tw`items-end`}>
                 {
                     chats ?
-                    <View style={tw`items-end`}>
-                    <View style={tw`h-[1.4rem] w-[1.4rem] bg-[${theme.yellow}] rounded-full items-center justify-center text-gray`}>
-                        <Text>2</Text>
-                    </View> 
-                    <Text style={tw`text-[${theme.fade_text}] text-[.8rem] mt-2`}>12:00 PM</Text>
-                    </View>
-                    :
-                    <AntDesign name="phone" size={24} color={theme.yellow} />
+                        <View style={tw`items-end`}>
+                            {
+                                newChat &&
+                                <View style={tw`h-[1.4rem] w-[1.4rem] bg-[${theme.yellow}] rounded-full items-center justify-center text-gray`}>
+                                    <Text>{count}</Text>
+                                </View>
+                            }
+                            <Text style={tw`text-[${theme.fade_text}] text-[.8rem] mt-2`}>12:00 PM</Text>
+                        </View>
+                        :
+                        <AntDesign name="phone" size={24} color={theme.yellow} />
                 }
             </View>
         </TouchableOpacity>
